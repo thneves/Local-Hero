71669e74d3e676700e7fc71b3137edfb026546ce
thneves <tsneves11@gmail.com>
2021-05-18 13:28:56 -0300

    Add logo, main files and folders

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'phaser';
+import config from './config/config';
+import GameScene from './scenes/GameScene';
 
-import { SimpleScene } from './scenes/simple-scene';
+class Game extends Phaser.Game {
+  constructor(){
+    super(config);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
+}
 
-const config = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
-};
-
-new Phaser.Game(config);
+window.game = new Game();
 
